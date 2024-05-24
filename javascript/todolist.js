fea65122b8e212f219d7cd6bec9885bf45b4c580
Danbi <danbi5033@naver.com>
2024-05-25 02:43:15 +0900

    add remove todo by double-clicking todo in list

--- javascript/todolist.js
+++ javascript/todolist.js
@@ -25,24 +25,39 @@ const loadList = (page) => {
     for (let j = 0; j < pageSize; j++) {
         const getTodo = todoLocalStorage.getTodoByIdx(i + j);
         if (getTodo) {
-            const newLi = document.createElement("li");
-            newLi.innerText = getTodo;
+            const newLi = document.createElement('li');
+            const checkBox = document.createElement('input');
+            checkBox.type = 'checkbox';
+            newLi.appendChild(checkBox);
+            newLi.append(" " + getTodo);
+            newLi.value = i + j;
             eventTargets.list.appendChild(newLi);
         }
     }
 };
 
+const isEmptyPage = (page) => {
+    const pageNum = Number(page);
+    const i = (pageNum - 1) * pageSize;
+
+    return todoLocalStorage.getTodoByIdx(i);
+};
+
 const refrashList = (nowPage) => {
     deleteList(eventTargets.list.children);
-    loadList(nowPage);
+    if (isEmptyPage(nowPage))
+        loadList(nowPage);
+    else {
+        nowPage = nowPage - 1;
+        loadList(nowPage);
+    }
 };
 
 class todoLocalStorage {
     constructor() {
         if (!localStorage.getItem("todos"))
             localStorage.setItem("todos", JSON.stringify(todoContents));
-        else
-            refrashList(1);
+        else refrashList(1);
     }
 
     static getAllTodo() {
@@ -57,6 +72,12 @@ class todoLocalStorage {
     static addTodo(todo) {
         const nowTodos = JSON.parse(localStorage.getItem("todos"));
         nowTodos.push(todo);
+        localStorage.setItem("todos", JSON.stringify(nowTodos));
+    }
+
+    static removeTodo(idx) {
+        const nowTodos = JSON.parse(localStorage.getItem("todos"));
+        nowTodos.splice(idx, 1);
         localStorage.setItem("todos", JSON.stringify(nowTodos));
     }
 }
@@ -103,4 +124,15 @@ eventTargets.input.addEventListener("keydown", (event) => {
         refrashList(nowPage);
     }
 });
+
+eventTargets.list.addEventListener("dblclick", (event) => {
+    if (event.target.nodeName === "LI") {
+        const idx = event.target.value;
+        event.target.parentNode.removeChild(event.target);
+        todoLocalStorage.removeTodo(idx);
+        pagenation(todoLocalStorage.getAllTodo());
+        refrashList(nowPage);
+    }
+});
+
 
